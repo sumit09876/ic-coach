8ec91581be702905c98cd91f79849878bdc27b04
sumit <sumit.lal007@gmail.com>
2024-01-01 18:20:08 +0530

    test001

--- bump_version.sh
+++ bump_version.sh
@@ -6,3 +6,5 @@ flutter pub run version
 # Commit the changes
 git add pubspec.yaml
 git commit -m "Bump version"
+git config --global user.email "sumit.lal007@gmail.com"
+git config --global user.name "sumit113"
