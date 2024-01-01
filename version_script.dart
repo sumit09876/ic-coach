6fb4d0d220e134b6c4d1333ad1b2ff0e5432e5bc
sumit <sumit.lal007@gmail.com>
2024-01-01 17:12:06 +0530

    test001

---
+++ version_script.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'package:version/version.dart';
+
+void main() {
+  // Read the current version from pubspec.yaml
+  final pubspecFile = File('pubspec.yaml');
+  final pubspecContent = pubspecFile.readAsStringSync();
+  final versionMatch = RegExp(r'version: (\d+\.\d+\.\d+)').firstMatch(pubspecContent);
+
+  if (versionMatch != null) {
+    final currentVersion = Version.parse(versionMatch.group(1)!);
+
+    // Increment the version
+    final newVersion = currentVersion.nextPatch;
+
+    // Update pubspec.yaml with the new version
+    final newPubspecContent = pubspecContent.replaceFirst(
+      versionMatch.group(0)!,
+      'version: $newVersion',
+    );
+    pubspecFile.writeAsStringSync(newPubspecContent);
+
+    print('Version bumped to $newVersion');
+  } else {
+    print('Error: Unable to find current version in pubspec.yaml');
+  }
+}
